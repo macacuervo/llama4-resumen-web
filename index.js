6c6acb188e965dc8da4e397a3b4aed18ac86e7aa
macacuervo <128818852+macacuervo@users.noreply.github.com>
2025-04-07 10:45:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ import { dirname } from "path";
 dotenv.config();
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
